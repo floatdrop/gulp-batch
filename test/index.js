c74aeedd655e683135abc8662397db11ec451c90
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2014-01-03 18:21:20 +0600

    Added tests on options

--- test/index.js
+++ test/index.js
@@ -16,12 +16,32 @@ describe('glob-batch', function () {
     });
 
     it('should batch async calls to array', function (done) {
-        var reciever = batch(function (events) {
+        var reciever = batch({ timeout: 200 }, function (events) {
             assert.equal(events.length, 2);
             done();
         });
         reciever('one');
         setTimeout(reciever.bind(null, 'two'), 100);
+    });
+
+    it('should flush, if we exceed timeout', function (done) {
+        var flushes = 0;
+        var reciever = batch({ timeout: 100 }, function (events) {
+            assert.equal(events.length, 1);
+            flushes += 1;
+            if (flushes === 2) { done(); }
+        });
+        reciever('one');
+        setTimeout(reciever.bind(null, 'two'), 200);
+    });
+
+    it('should flush, if we exceed limit', function (done) {
+        var reciever = batch({ limit: 2 }, function (events) {
+            if (events.length === 2) { done(); }
+        });
+        reciever('one');
+        reciever('two');
+        reciever('three');
     });
 
     it('should throw, if we provide invalid callback', function () {
