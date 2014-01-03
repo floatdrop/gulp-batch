bce898ddba97d907e52a16f210c7bb3286d37b3d
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2014-01-04 03:14:31 +0600

    Update README.md

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,12 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var batch = require('./index.js');
 
-function noop() {}
-
-gulp.task('mocha', function (cb) {
-    gulp.src(['test/*.js'])
+gulp.task('mocha', function () {
+    return gulp.src(['test/*.js'])
         .pipe(mocha({ reporter: 'list' }))
         .on('error', function (err) {
             console.log(err.stack);
-            cb();
-        })
-        .on('end', cb);
+        });
 });
 
 gulp.task('watch', function () {
